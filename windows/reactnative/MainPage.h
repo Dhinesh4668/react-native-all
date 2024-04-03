7c304f4ff7a0e4ac30af374f915b83c0c87572ad
Dhinesh Kumar <dhinesh4668@outlook.com>
2024-04-03 22:38:37 +0530

    config commit

---
+++ windows/reactnative/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::reactnative::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::reactnative::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
